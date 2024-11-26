8573e38bc8710aafe7099d06af5b69705c0fac6e
alba0403 <2024_alba.melendres@iticbcn.cat>
2024-11-26 23:18:31 +0100

    exercicis fets

--- Activitat3/A3exercici2.py
+++ Activitat3/A3exercici2.py
@@ -1,5 +1,6 @@
 # Sumar els primers 10 números utilitzant for i range().
 
 nombres = 1
+
 for i in range(1,11):
     print(i)
--- Activitat3/A3exercici6.py
+++ Activitat3/A3exercici6.py
@@ -1,1 +1,11 @@
 # Sumar números fins a arribar a 100 amb while. Caldrà sumar els números que estan inclosos entre 0 i 100. El programa deixarà d’executar-se quan s’arribi al número 100.
+
+suma = 0
+nombre = 0
+
+while nombre >= 0 and nombre <= 100 and suma <= 100:
+    nombre = int(input("Introdueix un valor: "))
+    suma = suma + nombre
+    if suma <= 100:
+        print(suma)
+    
