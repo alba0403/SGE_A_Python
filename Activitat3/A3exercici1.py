fbe2e4353f08cc101bf498cfb62daa9eb1ac50bb
alba0403 <2024_alba.melendres@iticbcn.cat>
2024-11-26 11:38:46 +0100

    canvis

--- Activitat3/A3exercici1.py
+++ Activitat3/A3exercici1.py
@@ -1,1 +1,14 @@
 # Imprimir números de l’1 al 10 (amb for i amb while)
+
+# Utilitzant for:
+
+nombres = [1,2,3,4,5,6,7,8,9,10]
+for i in nombres:
+    print(i)
+    
+# Utilitzant while:
+
+nombre = 1
+while nombre <= 10:
+   print(nombre)
+   nombre = nombre + 1
