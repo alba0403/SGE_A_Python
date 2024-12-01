0283c28e26e9b4c356aa796ce4a95d72b7b244af
alba0403 <2024_alba.melendres@iticbcn.cat>
2024-12-01 15:25:06 +0100

    Activitat 4 feta

--- Activitat4/main.py
+++ Activitat4/main.py
@@ -1,5 +1,6 @@
-
+# Importar les dues classes que hem fet (Cotxe i Colibri)
 from Cotxe import Cotxe
+from Colibri import Colibri
 
 # Fer tres instàncies de l'objecte Cotxe
 cotxe_1 = Cotxe("Blau", "Ford", 5, "Fiesta", 20000)
@@ -23,12 +24,17 @@ print(f'La seva mida és de {colibri_1.getMida()} cm.')
 print(f'I pesa uns {colibri_1.getPes()} grams.')
 
 # Modificar 2 atributs de Cotxe a través dels setters
-
+cotxe_1.setColor("Vermell")
+cotxe_2.setPreu(30000)
 
 # Mostrar 2 atributs modificats de Cotxe a través dels getters
-
+print(f'El meu cotxe és de color: {cotxe_1.getColor()}')
+print(f'El meu cotxe val: {cotxe_2.getPreu()}')
 
 # Modificar 2 atributs de Colibrí a través dels setters
-
+colibri_1.setColor("Vermell")
+colibri_2.setEdat(6)
 
 # Mostrar 2 atributs modificats de Colibrí a través dels getters
+print(f'El colibrí és de color {colibri_1.getColor()}')
+print(f'Té {colibri_1.getEdat()} anys.')
